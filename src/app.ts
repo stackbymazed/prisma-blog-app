9ebc2f36cbd1b05e48609073da648f12fef15d7a
Nayem783982 <majedulislam223311@gmail.com>
2025-12-31 21:39:29 +0600

    create a express server and setup modllar pattern file system

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+    console.log("server is running");
+})
+
+export default app;
+
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app";
+import { prisma } from "./lib/prisma"
+
+const port = process.env.PORT || 3000
+
+const main = async () => {
+    try {
+        await prisma.$connect()
+        console.log("successfully Prisma connected !");
+
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+
+    } catch (err) {
+        console.error(err)
+        await prisma.$disconnect()
+        process.exit(1)
+    }
+}
+
+main()
