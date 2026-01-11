80153e0bb87fa5e0125ef0c8eb941d0c8fa6314a
Nayem783982 <majedulislam223311@gmail.com>
2026-01-11 14:03:03 +0600

    implementing the auth middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { toNodeHandler } from 'better-auth/node'
 import { auth } from './lib/auth'
 import { PostRouter } from './modules/post/post.route'
 import "dotenv/config";
 
-
 const app = express()
 app.use(express.json())
+
+const Auth = () => {
+    async (req:Request, res:Response, next:NextFunction) => {
+        
+    }
+}
+
 //better auth authentication route
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
@@ -14,7 +20,7 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 
 
 //post create
-app.use("/posts",PostRouter.router)
+app.use("/posts", PostRouter.router)
 
 
 app.get("/", (req: Request, res: Response) => {
