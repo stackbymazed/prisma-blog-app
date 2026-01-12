b7f710e1d4879d9726f4570aef096387a2ee2881
Nayem783982 <majedulislam223311@gmail.com>
2026-01-12 22:40:23 +0600

    change

--- src/modules/post/post.route.ts
+++ src/modules/post/post.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { PostController } from "./post.controller";
+import auth, { UserRole } from "../../middlewares/auth";
 
 const router = express.Router()
 
-router.post("/",PostController.CreatePost)
+router.post("/",auth(UserRole.ADMIN,UserRole.USER), PostController.CreatePost)
 
 export const PostRouter = {
     router
